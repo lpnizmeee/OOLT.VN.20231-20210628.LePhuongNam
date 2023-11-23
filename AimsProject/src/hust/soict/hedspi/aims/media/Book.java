5a0aa1dbf531c375adbb4586cb6844bba379c9b3
lpnizmeee <lpnizmeee@lpnizmees-MacBook.local>
2023-11-23 14:48:43 +0700

    complete section 10.1 create disc class

--- AimsProject/src/hust/soict/hedspi/aims/media/Book.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -1,62 +1,17 @@
 package hust.soict.hedspi.aims.media;
+import hust.soict.hedspi.aims.media.Media;
 
-public class Book {
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
+public class Book extends Media{
     private List<String> authors = new ArrayList<String>();
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
 
     public List<String> getAuthors() {
         return authors;
-    }
-
-    public void setAuthors(List<String> authors) {
-        this.authors = authors;
     }
 
     public Book() {
 
     }
 
-    public Book(int id, String title, String category, float cost, List<String> authors) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-        this.authors = authors;
-    }
     public void addAuthor(String authorName) {
         if (authors.contains(authorName)) {
             System.out.println("Author already exists");
---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Disc.java
@@ -0,0 +1,7 @@
+package hust.soict.hedspi.aims.media;
+
+public class Disc extends Media {
+    protected int length;
+    protected String director;
+    super();
+}
